df632a6223369140c5c1dab91404134e55fb332b
jonathanvdc <jonathan.vdc@outlook.com>
2019-12-24 21:04:47 -0500

    Pass server to commands

--- src/bot.py
+++ src/bot.py
@@ -2,13 +2,15 @@
 
 import praw
 import json
+import time
+from accounting import InMemoryServer
 
 # A list of the commands accepted by the bot. Every command
 # is essentially a function that maps a message to a reply.
 # For convenience, every command is associated with a help
 # string here.
 COMMANDS = {
-    'help': ('prints a help message.', lambda msg: get_help_message(msg.author.name))
+    'help': ('prints a help message.', lambda msg, server: get_help_message(msg.author.name))
 }
 
 def read_config():
@@ -27,7 +29,7 @@ def create_reddit(config):
 
 def list_commands():
     """Creates a list of all commands accepted by this bot."""
-    return ['`%s` -- %s' % (cmd, COMMANDS[cmd][0]) for cmd in sorted(COMMANDS)]
+    return ['`%s` &ndash; %s' % (cmd, COMMANDS[cmd][0]) for cmd in sorted(COMMANDS)]
 
 def list_commands_as_markdown():
     """Creates a list of all commands accepted by this bot and formats it as Markdown."""
@@ -48,28 +50,32 @@ def reply(message, body):
     message.mark_read()
     return message.author.message(title, body)
 
-def process_message(message):
+def process_message(message, server):
     """Processes a message sent to the bot."""
     split_msg = message.body.split()
     if len(split_msg) == 0:
         reply(
             message,
-            'Hi %s! You sent me an empty message. Here\'s a list of the commands I do understand:\n\n%s' %
+            'Hi %s! You sent me an empty message. Here\'s a list of commands I do understand:\n\n%s' %
             (message.author.name, list_commands_as_markdown()))
     elif split_msg[0] in COMMANDS:
-        reply(message, COMMANDS[split_msg[0]](message))
+        reply(message, COMMANDS[split_msg[0]](message, server))
     else:
         reply(
             message,
-            'Hi %s! I didn\'t quite understand command your command \'%s\'. Here\'s a list of the commands I do understand:\n\n%s' %
+            'Hi %s! I didn\'t quite understand command your command `%s`. Here\'s a list of commands I do understand:\n\n%s' %
             (message.author.name, split_msg[0], list_commands_as_markdown()))
 
-def process_all_messages(reddit):
+def process_all_messages(reddit, server):
     """Processes all unread messages received by the bot."""
     for message in reddit.inbox.unread(limit=None):
-        process_message(message)
+        process_message(message, server)
 
 if __name__ == '__main__':
     reddit = create_reddit(read_config())
-    process_all_messages(reddit)
-    print("Achieved inbox zero!")
+    server = InMemoryServer()
+    while True:
+        # Process messages.
+        process_all_messages(reddit, server)
+        # Sleep for five seconds.
+        time.sleep(5)
