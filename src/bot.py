149fd9dadbcbedcb00957ed1286d1034d841a16a
jonathanvdc <jonathan.vdc@outlook.com>
2019-12-27 21:10:39 -0500

    Upgrade Reddit-only bot to Reddit/Discord bot

--- src/bot.py
+++ src/bot.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 
 import praw
+import discord
 import json
 import time
+import asyncio
 from accounting import LedgerServer, Authorization
 from commands import COMMANDS, list_commands_as_markdown, CommandException, assert_authorized, process_command
 
@@ -40,20 +42,48 @@ def process_all_messages(reddit, server):
 
     # TODO: process comments where the bot is mentioned.
 
-if __name__ == '__main__':
-    reddit = create_reddit(read_config())
+async def reddit_loop(reddit, server):
+    """The bot's main Reddit loop."""
     # Let's make a tick five minutes for now (should be way longer later).
     tick_duration = 5 * 60
+
+    while True:
+        # Process messages.
+        process_all_messages(reddit, server)
+
+        # Notify the server that one or more ticks have elapsed if necessary.
+        time_diff = int(time.time() - server.last_tick_timestamp)
+        if time_diff > tick_duration:
+            for i in range(time_diff // tick_duration):
+                server.notify_tick_elapsed()
+
+        # Sleep for five seconds.
+        await asyncio.sleep(5)
+
+if __name__ == '__main__':
+    config = read_config()
+    reddit = create_reddit(config)
+    discord_client = discord.Client()
+
+    # This is our main message callback.
+    @discord_client.event
+    async def on_message(message):
+        if message.author == discord_client.user:
+            return
+
+        content = message.content.lstrip()
+        if content.startswith('$tau '):
+            await message.channel.send(
+                '<@%s> %s' % (
+                    message.author.id,
+                    process_command(
+                        str(message.author.id),
+                        content[len('$tau '):].lstrip(),
+                        server)))
+
+        if message.content.startswith('$hello'):
+            await message.channel.send('Hello!')
+
     with LedgerServer('ledger.txt') as server:
-        while True:
-            # Process messages.
-            process_all_messages(reddit, server)
-
-            # Notify the server that one or more ticks have elapsed if necessary.
-            time_diff = int(time.time() - server.last_tick_timestamp)
-            if time_diff > tick_duration:
-                for i in range(time_diff // tick_duration):
-                    server.notify_tick_elapsed()
-
-            # Sleep for five seconds.
-            time.sleep(5)
+        asyncio.get_event_loop().create_task(reddit_loop(reddit, server))
+        discord_client.run(config['discord_token'])
