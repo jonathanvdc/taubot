6d9c0ffbad444c5f6934e5ee1cdc8eecfdef1095
gamingdiamond982 <30845740+gamingdiamond982@users.noreply.github.com>
2020-05-11 14:59:45 +0100

    converted all discord account names to mentions

--- src/utils.py
+++ src/utils.py
@@ -24,5 +24,26 @@ def split_into_chunks(message: bytes, max_length):
 
 def discord_postprocess(message: str) -> str:
     """Postprocesses a message for the Discord platform. This entails
-       replacing double newlines with single newlines."""
-    return message.replace('\n\n', '\n')
+       replacing double newlines with single newlines this also entails
+       turning discord account names into mentions"""
+    message = message.replace('\n\n', '\n')
+    lines = message.split('\n')
+    line_index = 0
+    for line in lines:
+        words = line.split()
+        index = 0
+        for word in words:
+            if word.startswith('discord/'):  # the name command will still run because the result is enclosed in back ticks so this would be read as `discord/ and not discord/
+                words[index] = f'<@{word.strip("discord/")}>'
+            index += 1
+        line = ''
+        for word in words:
+            line += word + ' '
+        lines[line_index] = line
+        line_index += 1
+
+
+    message = ''
+    for line in lines:
+        message += line + '\n'
+    return message
