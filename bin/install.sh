7e27f51f9d59085d25bb8b38343e18e4950cfec1
gamingdiamond982 <oj.nologic@gmail.com>
2021-01-05 12:52:45 +0000

    fixed install script

--- bin/install.sh
+++ bin/install.sh
@@ -7,13 +7,8 @@ echo "installing dependencies"
 apt-get install postgresql libpq-dev postgresql-client postgresql-client-common
 
 echo "setting up the database"
-if [[ $# -eq 0 ]]
-then
-  sudo -u postgres createuser taubot -dP
-else
-  sudo -u postgres createuser taubot -d
-  sudo -u postgres psql -c "ALTER USER taubot WITH PASSWORD '"$0"';"
-fi
+sudo -u postgres createuser taubot -dP
+
 sudo -u postgres createdb taubot
 sudo -u postgres createdb taubot-tests
 
